27023c4ed5e1b09e948dd0f05ad8db3a8ae7de87
Felipe Silva <contato.felipesilva2020@Hotmail.com>
2022-11-04 15:33:21 -0300

    Ending Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,27 @@
 //react, components, estáicos
-import { useContext } from "react"
+import { useContext, useEffect } from "react";
 import { QuizContext } from "./context/quiz";
 
-import Welcome from "./components/Welcome"
-import Question from './components/Question';
+import Welcome from "./components/Welcome";
+import Question from "./components/Question";
+import GameOver from "./components/GameOver";
 
-import "./App.css"
+import PickCategory from "./components/PickCategory";
+
+import "./App.css";
 
 function App() {
-  const [quizState, dispatch] = useContext(QuizContext)
+  const [quizState, dispatch] = useContext(QuizContext);
 
   return (
     <div className="App">
       <h1>Quiz de Programação</h1>
       {quizState.gameStage === "Start" && <Welcome />}
+      {quizState.gameStage === "Category" && <PickCategory />}
       {quizState.gameStage === "Playing" && <Question />}
+      {quizState.gameStage === "End" && <GameOver />}
     </div>
-  )
+  );
 }
 
 export default App
